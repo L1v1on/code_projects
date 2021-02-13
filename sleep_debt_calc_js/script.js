e670989d92e50d030d38da77d47330787581e886
Ezra Vance <3zravance@gmail.com>
2021-02-13 09:02:15 -0500

    sleep debt code refactor

--- sleep_debt_calc_js/script.js
+++ sleep_debt_calc_js/script.js
@@ -1,59 +1,23 @@
-function getSleepHours(day) {
-  switch (day) {
-    case 'monday':
-      return 8;
-      break;
-
-    case 'tuesday':
-      return 2;
-      break;
-
-    case 'wednesday':
-      return 3;
-      break;
-
-    case 'thursday':
-      return 4;
-      break;
-
-    case 'friday':
-      return 5;
-      break;
-
-    case 'saturday':
-      return 6;
-      break;
-
-    case 'sunday':
-      return 7;
-      break;
-
-    default:
-      return 0;
-      break;
-  }
-}
-
 const getActualSleepHours = () =>
-  getSleepHours('monday') +
-  getSleepHours('tuesday') +
-  getSleepHours('wednesday') +
-  getSleepHours('thursday') +
-  getSleepHours('friday') +
-  getSleepHours('saturday') +
-  getSleepHours('sunday');
+  8 +
+  2 +
+  3 +
+  4 +
+  5 +
+  6 +
+  7;
 
 console.log('Acutal Sleep Hours: ' + getActualSleepHours());
 
-function getIdealSleepHours() {
-  let idealHours = 8;
+function getIdealSleepHours(hours) {
+  let idealHours = hours;
   return idealHours * 7;
 }
-console.log('Ideal Sleep Hours: ' + getIdealSleepHours());
+console.log('Ideal Sleep Hours: ' + getIdealSleepHours(9));
 
 function calculateSleepDebt() {
   let actualSleepHours = getActualSleepHours();
-  let idealSleepHours = getIdealSleepHours();
+  let idealSleepHours = getIdealSleepHours(9);
   if (actualSleepHours == idealSleepHours) {
     console.log('You got perfect sleep!');
   } else if(actualSleepHours > idealSleepHours) {
