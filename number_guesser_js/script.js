9dc8b14127cadec99273d71eac22cf07dc784860
Ezra Vance <3zravance@gmail.com>
2021-02-22 12:19:27 -0500

    extreme guess conditions completed

--- number_guesser_js/script.js
+++ number_guesser_js/script.js
@@ -3,4 +3,20 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
+function generateTarget(){
+    return Math.floor(Math.random()*10);
+}
+//console.log(generateTarget());
 
+function compareGuesses(humanGuess, computerGuess, target){
+    if(humanGuess === target && computerGuess === target){
+        return true;
+    }
+    if(humanGuess === target){
+        return true;
+    }
+    if(computerGuess === target){
+        return false;
+    }
+    
+}
