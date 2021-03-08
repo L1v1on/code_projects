f6eab3b3c94341886a4e5808e2341625716e73d6
Ezra Vance <3zravance@gmail.com>
2021-03-08 11:13:46 -0500

    format update

--- whale_talk_js/script.js
+++ whale_talk_js/script.js
@@ -3,20 +3,20 @@ let vowels = ['A', 'E', 'I', 'O', 'U'];
 let resultArray = [];
 
 // check input for vowels
-for(let i = 0; i < input.length; i++){
-    //console.log(input[i]);
-    for(let j = 0; j < vowels.length; j++){
-        //console.log(vowels[j]);
-        if(input[i].toLocaleUpperCase() === vowels[j]){
-            resultArray.push(vowels[j]);
-            if(vowels[j] === 'E'){
-                resultArray.push(vowels[j]);
-            }
-            if (vowels[j] === 'U') {
-              resultArray.push(vowels[j]);
-            }
-            break;
-        }
+for (let i = 0; i < input.length; i++) {
+  //console.log(input[i]);
+  for (let j = 0; j < vowels.length; j++) {
+    //console.log(vowels[j]);
+    if (input[i].toLocaleUpperCase() === vowels[j]) {
+      resultArray.push(vowels[j]);
+      if (vowels[j] === 'E') {
+        resultArray.push(vowels[j]);
+      }
+      if (vowels[j] === 'U') {
+        resultArray.push(vowels[j]);
+      }
+      break;
     }
+  }
 }
 console.log(resultArray.join(''));
