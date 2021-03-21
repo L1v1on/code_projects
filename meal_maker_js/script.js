07281cef09b00906ec7cd29f486918cc174d4b7e
Ezra Vance <3zravance@gmail.com>
2021-03-20 23:36:03 -0400

    _courses property setup w/ getters & setters.

--- meal_maker_js/script.js
+++ meal_maker_js/script.js
@@ -1,3 +1,34 @@
 const menu = {
-    
-}
+  _courses: {
+    appetizers: [],
+    mains: [],
+    desserts: [],
+  },
+  get appetizers() {
+    return this._courses.appetizers;
+  },
+  get mains() {
+    return this._courses.mains;
+  },
+  get desserts() {
+    return this._courses.desserts;
+  },
+  set appetizers(appetizer) {
+    this._courses.appetizers = appetizer;
+  },
+  set mains(main) {
+    this._courses.mains = main;
+  },
+  set desserts(dessert) {
+    this._courses.desserts = dessert;
+  },
+  get courses() {
+    return {
+      appetizers: this._courses.appetizers,
+      mains: this._courses.mains,
+      desserts: this._courses.desserts,
+    };
+  },
+};
+
+console.log(menu.courses);
