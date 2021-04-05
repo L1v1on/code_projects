ceee5350d4db955d326f8bdffc664dcaee8d9ae6
Ezra Vance <3zravance@gmail.com>
2021-04-05 16:01:35 -0400

    3 player objects added

--- team_stats_js/script.js
+++ team_stats_js/script.js
@@ -1,4 +1,20 @@
 const team = {
-    _players: [],
-    _games: [],
-}
+  _players: [
+    {
+      firstName: 'Pablo',
+      lastName: 'Sanchez',
+      age: 11,
+    },
+    {
+      firstName: 'Bob',
+      lastName: 'Joe',
+      age: 13,
+    },
+    {
+      firstName: 'Tom',
+      lastName: 'Dill',
+      age: 10,
+    },
+  ],
+  _games: [],
+};
