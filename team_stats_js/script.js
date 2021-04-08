f6bc0f18c4d744b22d03088cd09d1471b1557fff
Ezra Vance <3zravance@gmail.com>
2021-04-08 13:36:54 -0400

    addGame method added and tested

--- team_stats_js/script.js
+++ team_stats_js/script.js
@@ -33,20 +33,30 @@ const team = {
       opponentPoints: 15,
     },
   ],
-  get players(){
+  get players() {
     return this._players;
   },
-  get games(){
+  get games() {
     return this._games;
   },
-  addPlayer(firstName, lastName, age){
+  addPlayer(firstName, lastName, age) {
     let player = {
       firstName: firstName,
       lastName: lastName,
       age: age,
     };
     this._players.push(player);
-  }
+  },
+  addGame(opponent, teamPoints, opponentPoints) {
+    let game = {
+      opponent: opponent,
+      teamPoints: teamPoints,
+      opponentPoints: opponentPoints,
+    };
+    this._games.push(game);
+  },
 };
-//team.addPlayer('Ezra', 'Vance', 99);
-//console.log(team.players);
+team.addPlayer('Ezra', 'Vance', 99);
+console.log(team.players);
+team.addGame('Noodle', 30, 60);
+console.log(team.games);
