dce89d5526e287e88d0c70d75ddf4e08263e4ab6
Ezra Vance <3zravance@gmail.com>
2021-08-31 10:15:02 -0400

    willLikelySurvive method implemented and tested.

--- mystery-organism-starter/main.js
+++ mystery-organism-starter/main.js
@@ -14,29 +14,41 @@ const mockUpStrand = () => {
 };
 
 // Factory funtion
-function pAequorFactory(num, dnaBase){
+function pAequorFactory(num, dnaBase) {
   let pAequor = {};
   pAequor.specimenNum = num;
   pAequor.dna = dnaBase;
   pAequor.mutate = function () {
     let randI = Math.floor(Math.random() * 15);
     let randBase = returnRandBase();
-    if(this.dna[randI] == randBase){
-      do{
+    if (this.dna[randI] == randBase) {
+      do {
         randBase = returnRandBase();
-      }while(this.dna[randI] == randBase)
+      } while (this.dna[randI] == randBase);
     }
     this.dna[randI] = randBase;
     return this.dna;
   };
   pAequor.compareDNA = function (pAequor) {
     let identical = 0;
-    for(x = 0; x < this.dna.length; x++){
-      if(this.dna[x] == pAequor.dna[x]) identical++;
+    for (x = 0; x < this.dna.length; x++) {
+      if (this.dna[x] == pAequor.dna[x]) identical++;
     }
-    let percent = ((identical / 15)  * 100).toFixed(3);
+    let percent = ((identical / 15) * 100).toFixed(3);
 
-    console.log(`Specimen #${this.specimenNum} and specimen #${pAequor.specimenNum} have ${percent}% DNA in common!`);
+    console.log(
+      `Specimen #${this.specimenNum} and specimen #${pAequor.specimenNum} have ${percent}% DNA in common!`
+    );
+  };
+  pAequor.willLikelySurvive = function () {
+    let c = 0;
+    let g = 0;
+    this.dna.forEach((base) => {
+      if (base == 'C') c++;
+      if (base == 'G') g++;
+    });
+    if (c >= 9 || g >= 9) return true;
+    return false;
   };
   return pAequor;
 }
@@ -44,6 +56,18 @@ function pAequorFactory(num, dnaBase){
 let testDNA1 = pAequorFactory(1, mockUpStrand());
 let testDNA2 = pAequorFactory(2, mockUpStrand());
 testDNA1.compareDNA(testDNA2);
+console.log(
+  `Will specimen ${
+    testDNA1.specimenNum
+  } survive? ${testDNA2.willLikelySurvive()}`
+);
+console.log(
+  `Will specimen ${
+    testDNA2.specimenNum
+  } survive? ${testDNA2.willLikelySurvive()}`
+);
 /* console.log(testDNA);
 console.log('Activate mutation');
-console.log(testDNA.mutate()); */
+console.log(testDNA.mutate()); 
+testDNA3 = pAequorFactory(3, ['G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G']);
+console.log(testDNA3.willLikelySurvive()); */
