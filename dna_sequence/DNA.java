29ae9b298fec3712fe62d45b9976e62154d1caf2
Ezra Vance <3zravance@gmail.com>
2022-06-01 10:04:08 -0400

    add comment about class.

--- dna_sequence/DNA.java
+++ dna_sequence/DNA.java
@@ -1,5 +1,14 @@
 package dna_sequence;
 
+/* 
+ *   Class determines if DNA strand contains a protein.
+ *   A protein has these qualities:
+ *     1. Begins with start codon: "ATG"
+ *     2. Ends with stop codon: "TGA"
+ *     3. Between start and stop codons are additional codons.
+ *     4. Codons are a sequence of 3 nucleotides
+ */
+
 public class DNA {
 
     public static void main(String[] args) {
